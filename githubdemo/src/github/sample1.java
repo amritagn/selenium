45c26725cb54a5009be78ad589998f7b92bf8ee3
amrita <amritagovind14@gmail.com>
2023-04-10 12:52:54 +0530

    Addition of two numbers

--- githubdemo/src/github/sample1.java
+++ githubdemo/src/github/sample1.java
@@ -4,6 +4,11 @@ public class sample1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		int a=10;
+		int b=10;
+		System.out.println("Output");
+		System.out.println(a+b);
+		
 
 	}
 
